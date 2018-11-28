1ba824529fab04c188d9b77ba07dbece8535d977
be5 <beskhlebnyi@gmail.com>
2018-11-28 11:30:04 +0200

    add google api key and change location to Kiev

--- app/src/main/java/com/be5/ordnarymaps/MapsActivity.java
+++ app/src/main/java/com/be5/ordnarymaps/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng kiev = new LatLng(50, 30);
+        mMap.addMarker(new MarkerOptions().position(kiev).title("Marker in Kiev"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(kiev));
     }
 }
